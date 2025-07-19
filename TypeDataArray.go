45a03921d8c0dc68fc164a7e8e24e19559ed1928
pavelc4 <dimzzy23@gmail.com>
2025-07-20 03:33:31 +0700

    feat: Add example of array declaration with indexed data in Go
    This commit demonstrates how to declare and use a fixed-size array in Go. Each element is manually assigned using its index, and the contents are printed one by one.
    
    ### Array Behavior in Go:
    - Arrays are type-specific and fixed in size
    - Indexing starts from 0
    - Elements are accessed with `array[index]` notation
    
    ### Example Table:
    
    | Index | Data      |
    |-------|-----------|
    | 0     | manggo    |
    | 1     | orange    |
    | 2     | apple     |
    
    Arrays in Go are useful when the number of elements is known and constant. For dynamic data sets, slices are more appropriate.

---
+++ TypeDataArray.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var food [3]string
+
+	food[0] = "manggo"
+	food[1] = "orange"
+	food[2] = "apple"
+
+	fmt.Println(food[0])
+	fmt.Println(food[1])
+	fmt.Println(food[2])
+}
