1fe1ba3865170e9a0ca5b4858d10863fd28327a5
pavelc4 <dimzzy23@gmail.com>
2025-07-19 08:04:58 +0700

    Add example demonstrating how to declare and use variables in Go
    This commit includes a simple Go program that illustrates how variables are created, assigned values, and updated.
    
    The goal of this example is to help beginners understand how variable declarations work in Go.
    
    ### Concepts covered:
    
    - Declaring variables using the `var` keyword with explicit type
    - Declaring variables using `var` with type inference
    - Using short variable declaration syntax with `:=`
    - Assigning values to variables after declaration
    - Reassigning (updating) values to existing variables
    - Printing variable values to observe changes
    
    This is a basic but important foundation for learning Go, as variables are used in nearly every program. The example avoids complex logic and focuses purely on how to work with variables effective

---
+++ variabel.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var animals string
+
+	animals = "Cow"
+	fmt.Println(animals)
+
+	animals = "Ewe"
+	fmt.Println(animals)
+
+	var food = "Meetball"
+
+	fmt.Println(food)
+
+	food = "steak"
+	fmt.Println(food)
+
+	hobby := "playing games"
+	fmt.Println(hobby)
+
+	hobby = " football"
+	fmt.Println(hobby)
+
+}
